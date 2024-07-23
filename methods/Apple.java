3f92fb45f61b4469711aca09d4f8bc3003b8410d
bhavyacs2002 <bhavyashreecs.xworkz@gmail.com>
2024-07-23 21:04:28 +0530

    old object creation

--- methods/Apple.java
+++ methods/Apple.java
@@ -2,5 +2,9 @@ class Apple{
 	public static void eat()
 	{
 		System.out.println("running eat in apple");
+		for(int j,i=0;i<=10;i++)
+		{
+			
+		}
 	}
 }
--- win/Market.java
+++ win/Market.java
@@ -1,39 +1,7 @@
 class Market
 {
-	public static String product(double price)
+	public Market()
 	{
-		System.out.println("Running product in Market");
-		if(price<500 && price>0)
-		{
-			return "cheap";
-		}
-		else if (price>500)
-		{
-			return "costly";
-		}
-		return "invalid";
+		System.out.println("Market created");
 	}
-	public static String Phone(long phoneNo)
-	{
-		System.out.println("Running Phone in Market");
-		if (phoneNo==6362411289L)
-		{
-			return "available";
-		}
-		else{
-			return "not available";	
-		}
-	}
-	public static void doctor(boolean available)
-	{
-		System.out.println("Running doctor in Market");
-		if(available==true)
-		{
-			System.out.println("no return type");
-		}
-		else{
-			System.out.println("not availble");
-		}
-		return;
-	}		
-}                           
+}
